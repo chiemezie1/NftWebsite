f15e822de9d4cfd6942905fc423403f6a3fe9c15
chiemezie1 <chiemezieagbo1@gmail.com>
2023-07-20 20:04:26 +0100

    auth Controllerk

--- Api/Controllers/authController.js
+++ Api/Controllers/authController.js
@@ -8,7 +8,7 @@ const signToken = (id )=>{
 };
 
 const createSendToken = (user, statusCode, req, res) => {
-    const token = signToken(user.id);
+    const token = signToken(user._id);
 
     res.cookie("jwt", token, {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
