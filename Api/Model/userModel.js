398f1f5f523dfdcec942dd0de6e5cd9fe6d5f9b6
chiemezie1 <chiemezieagbo1@gmail.com>
2023-07-20 14:49:18 +0100

    user schema

---
+++ Api/Model/userModel.js
@@ -0,0 +1,77 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        Type: String,
+        required: [true, "Please provide your name"],
+    },
+    email: {
+        type: String,
+        required : [true, "Please provide your email"],
+        unique: true,
+        lowercase: true,
+    },
+    role: {
+        type: {
+            type: String,
+            enum: ["user", "admin"],
+            default: "user"
+        },
+        password: {
+            type: String,
+            required: [true, "please provide a password"],
+        },
+        passwordConfirm: {
+            type: String,
+            required: [true, "please confirm your password"],
+            Validate:{
+                validator: function(el){
+                    return el === this.password;
+                },
+                message: "password not the same"
+            },
+        },
+    },
+});
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 12);
+
+    this.passwordConfirm = undefined;
+
+    next();
+});
+
+userSchema.pre("save", function(next){
+    if (!this.isModified("password") || this.isNew ) return next();
+
+    this.passwordChangedAt = Date.now() - 1000;
+
+    next();
+});
+
+userSchema.pre(/^find/, function(next){
+    this.find({active: {$ne: false}});
+    next();
+});
+
+
+userSchema.method.correctPassword = async function(candidatePassword, userPassword){
+    return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.method.changedPasswordAfter = function(JWTTimestamp){
+    if (this.passwordChangedAt){
+        const changedTimeStamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10);
+        return JWTTimestamp < changedTimeStamp;
+    }
+
+    return false
+};
+
+const user = mongoose.model("user", userSchema);
+
+module.exports = user;
